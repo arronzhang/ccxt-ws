a8f92f2322f73da4ce2e57dc6cc697034be1260b
Arron <arronzhang@me.com>
2019-12-19 11:51:06 +0800

    Subscribe topic

--- test/huobipro-ws.test.js
+++ test/huobipro-ws.test.js
@@ -1,18 +1,40 @@
 const Exchange = require('../lib/huobipro-ws')
 const agent = require('./agent')
 
+let api
+
+beforeEach(() => {
+  api = new Exchange({ agent })
+})
+
+afterEach(() => {
+  api.wsClose()
+})
+
 describe('huobipro ws api', () => {
   test(
     'wsRequest',
     async () => {
-      const api = new Exchange({ agent })
       expect(api.wsRequest()).rejects.toThrow()
       await api.wsConnect()
       await expect(api.wsRequest({ sub: 'market.btcusdt.trade.detailerr' })).rejects.toThrow()
       await expect(api.wsRequest({ sub: 'market.btcusdt.trade.detail' })).resolves.toHaveProperty(
         'subbed'
       )
-      api.wsClose()
+    },
+    10 * 1000
+  )
+  test(
+    'subscribe',
+    async done => {
+      await api.wsConnect()
+      api.on('trade', (trade, market) => {
+        expect(trade.symbol).toBe('BTC/USDT')
+      })
+      api.on('trades', () => {
+        done()
+      })
+      await api.subscribeTrades('BTC/USDT')
     },
     10 * 1000
   )
