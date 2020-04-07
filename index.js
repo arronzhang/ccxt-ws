c13126214fd3350a1fd865c8936e0be82db68778
Arron <arronzhang@me.com>
2020-04-07 15:39:47 +0800

    Export only ws objects

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const ccxt = (module.exports = require('ccxt'))
-
 const exchanges = {
   huobiproWs: require('./lib/huobipro-ws.js')
 }
 
-Object.assign(
-  ccxt,
+module.exports = Object.assign(
   {
     ExchangeWs: require('./lib/exchange-ws.js'),
     exchanges: Object.keys(exchanges)
